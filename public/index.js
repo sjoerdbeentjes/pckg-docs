6703f85a561da3f6347c32e3cb71acf866787db1
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2020-03-11 21:58:35 +0100

    add search to sidebar

--- public/index.js
+++ public/index.js
@@ -1,13 +1,30 @@
 (function () {
   const scrollKey = 'sidebarScrollPosition';
   const sidebarEl = document.querySelector('.sidebar');
+  const searchInputEl = document.querySelector('input[type="search"]');
+  const packageEls = document.querySelectorAll('.sidebar ul li');
   const storedPosition = sessionStorage.getItem(scrollKey);
 
   if (storedPosition) {
     sidebarEl.scrollTop = Number(storedPosition);
   }
 
-  sidebarEl.addEventListener('scroll', (event) => {
+  sidebarEl.addEventListener('scroll', () => {
     sessionStorage.setItem(scrollKey, sidebarEl.scrollTop);
+  });
+
+  searchInputEl.addEventListener('input', (event) => {
+    const query = event.target.value.toLowerCase();
+
+    packageEls.forEach((packageEl) => {
+      const name = packageEl.getAttribute('data-name');
+      const match = name.includes(query.toLowerCase());
+
+      if (match) {
+        packageEl.classList.remove('hidden');
+      } else {
+        packageEl.classList.add('hidden');
+      }
+    });
   });
 }());
